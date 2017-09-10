692088e1bd250cc5e4d5cb8ac121ccddf5918e66
Shri-456 <30828674+Shri-456@users.noreply.github.com>
2017-09-10 18:39:33 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.H>
+void main()
+{
+char c;
+printf("/n enter a character");
+scanf("%c",c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+printf("/n it is a vowel");
+}
+else
+{
+printf("/n it is consonant");
+}
+}
